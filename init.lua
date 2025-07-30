740703f70423c87ba72d05a1ee1fdf9dce8f9914
MevyDev <mevydev@proton.me>
2025-07-30 19:07:13 +0200

    godot indentation functionality from vim-godot plugin

--- init.lua
+++ init.lua
@@ -991,6 +991,8 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  { 'habamax/vim-godot', event = 'VimEnter' },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
