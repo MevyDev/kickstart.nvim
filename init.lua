88ac1197b1d7a80fc898c87d7e9ec8665ecd2bf3
MevyDev <mevydev@proton.me>
2025-08-11 11:48:31 +0200

    add catppuccin plugin

--- init.lua
+++ init.lua
@@ -1009,6 +1009,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  -- custom plugins
+  require 'custom.plugins.catppuccin',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
