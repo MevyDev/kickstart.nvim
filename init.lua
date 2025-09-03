c2b1678c3daabd3f65db2a35f319f0448961d75e
MevyDev <mevydev@proton.me>
2025-09-03 05:44:53 +0200

    tab size to 4

--- init.lua
+++ init.lua
@@ -128,8 +128,8 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
-vim.o.tabstop = 8
-vim.o.shiftwidth = 8
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
 -- Keep signcolumn on by default
