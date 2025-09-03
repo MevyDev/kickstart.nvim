3525b4676bb65aab2289f5386c03c9dcb8f0a373
MevyDev <mevydev@proton.me>
2025-09-03 05:46:38 +0200

    cft for cp problems with testcases

--- snippets/cpp.lua
+++ snippets/cpp.lua
@@ -2,6 +2,34 @@ local ls = require 'luasnip'
 local s = ls.snippet
 
 return {
+  s('cft', {
+    t {
+      '#include <bits/stdc++.h>',
+      'using namespace std;',
+      'using ll = long long;',
+      '#define debug(x) cerr << #x << R"(: )" << x << endl;',
+      '#define ans(s) {cout << s << endl; return;}',
+      '#define rall(x) (x).rbegin(), (x).rend()',
+      '#define all(x) x.begin(), x.end()',
+      '#define pb push_back',
+      '#define fi first',
+      '#define se second',
+      '',
+      'void solve() {',
+      '    ',
+    },
+    i(0),
+    t {
+      '',
+      '}',
+      '',
+      'signed main() {',
+      '    // ios::sync_with_stdio(false); cin.tie(nullptr);',
+      '    int t; cin >> t;',
+      '    while(t--) solve();',
+      '}',
+    },
+  }),
   s('cf', {
     t {
       '#include <bits/stdc++.h>',
@@ -16,17 +44,16 @@ return {
       '#define se second',
       '',
       'void solve() {',
-      '        ',
+      '    ',
     },
     i(0),
     t {
       '',
       '}',
       '',
-      'int main() {',
-      '        // ios::sync_with_stdio(false); cin.tie(nullptr);',
-      '        int t; cin >> t;',
-      '        while(t--) solve();',
+      'signed main() {',
+      '    // ios::sync_with_stdio(false); cin.tie(nullptr);',
+      '    solve();',
       '}',
     },
   }),
